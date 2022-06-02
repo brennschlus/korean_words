f968b43c834a381463c13291019229624f4d2109
Pavel Rodionov <brennschluss@protonmail.com>
2022-06-02 21:10:02 +0300

    changed error prone aproach to count vector length by commas to new lines

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,18 @@
 
 module Main where
 
+import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv (FromNamedRecord (..), decodeByName, (.:))
 import qualified Data.Vector as V
 import qualified Data.Word8 as W
-import System.Random (randomRIO)
+import System.Random (Random (random), randomRIO)
 
 main :: IO ()
 main = do
   let fileName = "words.csv"
   csvData <- BL.readFile fileName
-  let vectorWithCommas = BL.filter (isComma) csvData
-  let length = (fromIntegral $ BL.length vectorWithCommas) - 2 :: Int
+  let length = countNewLines csvData - 2
   randomNumber <- randomRIO (0, length)
   case decodeByName csvData of
     Left err -> putStrLn err
@@ -29,7 +29,7 @@ data KoreanWord = KoreanWord
 instance FromNamedRecord KoreanWord where
   parseNamedRecord r = KoreanWord <$> r .: "word" <*> r .: "translation"
 
-isComma :: W.Word8 -> Bool
-isComma word = word == W._comma
+countNewLines :: BL.ByteString -> Int
+countNewLines byteString = length $ BL.split 10 byteString -- -- fromEnum '\n' == 10
 
 -- https://docs.google.com/spreadsheets/d/16pRPe1aNRoQbcihSBkGs5La5b59gQ0bCq1-ss09pgFI/edit#gid=0
